5ed302995327dff9e4ae5b94d9de5f252a4d161e
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2018-10-17 01:48:24 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,6 +1,10 @@
-n = int(input())
-c = 0
-while n!=0:
-    n = n//10
-    c+=1
-print(c)
+n = input()
+if n.isnumeric():
+    n = int(n)
+    c = 0
+    while n!=0:
+        n = n//10
+        c+=1
+    print(c)   
+else:
+    print('Invalid Input')
