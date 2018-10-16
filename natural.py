11f73334193d5f378fdddd3cba6091e2ad8f3190
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2018-10-17 02:01:53 +0530

    Update natural.py

--- natural.py
+++ natural.py
@@ -1,5 +1,8 @@
 n = int(input())
-su = 0
-for x in range(0,n+1):
-    su+=x
-print(su)    
+if n >= 0:
+    su = 0
+    for x in range(0,n+1):
+        su+=x
+    print(su) 
+else:
+    print('Invalid Input')
