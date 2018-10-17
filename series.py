3409598b8621f85e9a0f473976ad0b94990a2610
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2018-10-17 17:08:32 +0530

    Update series.py

--- series.py
+++ series.py
@@ -2,9 +2,9 @@ i,l = list(map(int,input().split()))
 if i < 100001 and l < 100001:
     if i%2 == 0:
         for x in range(i+1,l,2):
-            print(x)
+            print(x,end = ' ')
     else:
         for x in range(i+2,l,2):
-            print(x)            
+            print(x,end = ' ')            
 else:
-    print('Invalid Input') 
+    print('Invalid Input')   
