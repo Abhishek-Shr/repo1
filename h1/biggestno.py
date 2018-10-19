95404cbd985993f4aa9a5ef955355048a16726d4
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2018-10-19 17:31:57 +0530

    Update biggestno.py

--- h1/biggestno.py
+++ h1/biggestno.py
@@ -1,5 +1,9 @@
 n = int(input())
 li = list(map(int,input().split()))
-if n <100001 and n > 0 :    
-    s = ' '.join(map(str,sorted(li,reverse = True)))
-    print(s)
+if n <100001 and n > 0 :
+    # if li == [0]*len(li):
+    if all([v == 0 for v in li]):
+        print('0')                
+    else:
+        s = ''.join(map(str,sorted(li,reverse = True)))
+        print(s)
