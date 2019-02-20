5b5e3b054d2097c9557b9daf93ed20dabab6ea87
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2019-02-20 14:53:35 +0000

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Practise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int n = sc.nextInt();
+        System.out.println(n/60+" "+n%60);
+     }
+}
