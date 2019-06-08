1288a15a8667ebf6064d649de4c619f5a1f0c8f0
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2019-06-08 23:04:48 +0530

    Create RepeatNtimes.java

---
+++ beginner/RepeatNtimes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class RepeatNtimes{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        String s = sc.nextLine();
+        int n = sc.nextInt();
+        for(i=0;i<n;i++){
+            System.out.println(s);
+        }
+    }
+}
