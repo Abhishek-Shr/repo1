63e28d55a82597688c85a91172bb1610040a8efa
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2019-06-08 23:18:07 +0530

    Create StrConcat.java

---
+++ beginner/StrConcat.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class StrConcat{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        String s = sc.nextLine();
+        String r = sc.nextLine();
+        System.out.println(s+r);
+    }
+}
