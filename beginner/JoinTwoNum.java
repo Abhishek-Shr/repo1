777346414b030e1dd6cd0d5af5af3b491fe636af
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2019-06-08 11:52:39 +0530

    Create JoinTwoNum.java

---
+++ beginner/JoinTwoNum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class JoinTwoNum{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String[] str = s.split(" ");
+        StringBuffer sr = new StringBuffer();
+        sr.append(str[0]);
+        sr.append(str[1]);
+        String t =String.valueOf(sr);
+        System.out.println(t);
+
+
+    }
+}
