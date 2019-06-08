c9d199a512d3c97d8681259759dd5f16ad639e2a
Abhishek-Shr <32926683+Abhishek-Shr@users.noreply.github.com>
2019-06-08 11:46:15 +0530

    Update NPowK.java

--- beginner/NPowK.java
+++ beginner/NPowK.java
@@ -3,8 +3,8 @@ class NPowK{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i;        
-        int n = sc.nextInt();
-        int l = n;
+        long n = sc.nextInt();
+        int l = Integer.parseInt(String.valueOf(n));
         int k = sc.nextInt();
         for(i=1;i<k;i++){
             n = n*l;
